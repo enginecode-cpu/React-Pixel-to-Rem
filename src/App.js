19817a8ed0a3a5ec13b0a16c7d5f06e078a1878b
enginecode-cpu <bassmatics1002@gmail.com>
2021-01-20 19:52:54 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pixel, setPixel] = useState("");
+  const [rem, setRem] = useState("");
+  const [rootPixel, setRootPixel] = useState("16");
+
+  const handleRootPixelChange = (e) => {
+    setRootPixel(e.target.value);
+  };
+
+  const handlePixelChange = (e) => {
+    const pxValue = e.target.value; // string type
+    setPixel(pxValue);
+    const resultRem = parseFloat(pxValue) / parseFloat(rootPixel);
+    setRem(resultRem);
+  };
+
+  const handleRemChange = (e) => {
+    const remValue = e.target.value; // string type
+    setRem(remValue);
+    const resultPx = parseFloat(remValue) * parseFloat(rootPixel);
+    setPixel(resultPx);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="title">pixel to rem</h2>
+      <h2 className="root-pixel">
+        root pixel:{" "}
+        <input type="text" value={rootPixel} onChange={handleRootPixelChange} />
+      </h2>
+      <div className="container">
+        <div className="px-box">
+          <h3>Pixel</h3>
+          <input
+            type="text"
+            name="px"
+            value={isNaN(pixel) ? "" : pixel}
+            onChange={handlePixelChange}
+            placeholder="픽셀을 입력하세요."
+          />
+          <span className="px">px</span>
+        </div>
+        <div className="rem-box">
+          <h3>Rem</h3>
+          <input
+            type="text"
+            name="rem"
+            value={isNaN(rem) ? "" : rem}
+            onChange={handleRemChange}
+          />
+          <span className="rem">rem</span>
+        </div>
+      </div>
     </div>
   );
 }
