67572279027daf754a4f978fccdbcbbe3649da60
enginecode-cpu <bassmatics1002@gmail.com>
2021-01-21 11:46:12 +0900

    Add button and event

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
   const [pixel, setPixel] = useState("");
   const [rem, setRem] = useState("");
   const [rootPixel, setRootPixel] = useState("16");
@@ -24,35 +25,42 @@ function App() {
     setPixel(resultPx);
   };
 
+  const handleChangeCalc = (e) => {
+    setToggle(!toggle);
+  };
+
   return (
     <div className="App">
-      <h2 className="title">pixel to rem</h2>
+      <h2 className="title">{toggle ? "rem to pixel" : "pixel to rem"}</h2>
       <h2 className="root-pixel">
         root pixel:{" "}
         <input type="text" value={rootPixel} onChange={handleRootPixelChange} />
       </h2>
       <div className="container">
         <div className="px-box">
-          <h3>Pixel</h3>
+          <h3>{toggle ? "Rem" : "Pixel"}</h3>
           <input
             type="text"
             name="px"
             value={isNaN(pixel) ? "" : pixel}
-            onChange={handlePixelChange}
+            onChange={toggle ? handleRemChange : handlePixelChange}
             placeholder="픽셀을 입력하세요."
           />
           <span className="px">px</span>
         </div>
         <div className="rem-box">
-          <h3>Rem</h3>
+          <h3>{toggle ? "Pixel" : "Rem"}</h3>
           <input
             type="text"
             name="rem"
             value={isNaN(rem) ? "" : rem}
-            onChange={handleRemChange}
+            onChange={toggle ? handlePixelChange : handleRemChange}
           />
           <span className="rem">rem</span>
         </div>
+        <button className="change" onClick={handleChangeCalc}>
+          바꿔서 계산하기
+        </button>
       </div>
     </div>
   );
