c698d0da9d35fc87e9cffe61d2f443af9faba33f
enginecode-cpu <bassmatics1002@gmail.com>
2021-01-22 15:28:54 +0900

    Add button function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -7,59 +7,80 @@ function App() {
   const [rem, setRem] = useState("");
   const [rootPixel, setRootPixel] = useState("16");
 
-  const handleRootPixelChange = (e) => {
+  const handleRootPixelChange = useCallback((e) => {
     setRootPixel(e.target.value);
-  };
+  }, []);
 
-  const handlePixelChange = (e) => {
-    const pxValue = e.target.value; // string type
-    setPixel(pxValue);
-    const resultRem = parseFloat(pxValue) / parseFloat(rootPixel);
-    setRem(resultRem);
-  };
+  const handlePixelChange = useCallback(
+    (e) => {
+      const pxValue = e.target.value; // string type
+      setPixel(pxValue);
+      const resultRem = parseFloat(pxValue) / parseFloat(rootPixel);
+      setRem(resultRem);
+    },
+    [rootPixel]
+  );
 
-  const handleRemChange = (e) => {
-    const remValue = e.target.value; // string type
-    setRem(remValue);
-    const resultPx = parseFloat(remValue) * parseFloat(rootPixel);
-    setPixel(resultPx);
-  };
+  const handleRemChange = useCallback(
+    (e) => {
+      const remValue = e.target.value; // string type
+      setRem(remValue);
+      const resultPx = parseFloat(remValue) * parseFloat(rootPixel);
+      setPixel(resultPx);
+    },
+    [rootPixel]
+  );
 
-  const handleChangeCalc = (e) => {
+  const handleChangeCalc = useCallback(() => {
     setToggle(!toggle);
-  };
+  }, [toggle]);
+
+  const pixelInputValue = toggle
+    ? isNaN(rem)
+      ? ""
+      : rem
+    : isNaN(pixel)
+    ? ""
+    : pixel;
+  const remInputValue = toggle
+    ? isNaN(pixel)
+      ? ""
+      : pixel
+    : isNaN(rem)
+    ? ""
+    : rem;
 
   return (
     <div className="App">
-      <h2 className="title">{toggle ? "rem to pixel" : "pixel to rem"}</h2>
+      <h2 className="title">{toggle ? "Rem to Pixel" : "Pixel to Rem"}</h2>
       <h2 className="root-pixel">
         root pixel:{" "}
         <input type="text" value={rootPixel} onChange={handleRootPixelChange} />
       </h2>
       <div className="container">
-        <div className="px-box">
+        <div className={toggle ? "rem-box" : "px-box"}>
           <h3>{toggle ? "Rem" : "Pixel"}</h3>
           <input
             type="text"
-            name="px"
-            value={isNaN(pixel) ? "" : pixel}
+            value={pixelInputValue}
             onChange={toggle ? handleRemChange : handlePixelChange}
-            placeholder="픽셀을 입력하세요."
+            placeholder={toggle ? "" : "픽셀을 입력하세요."}
           />
-          <span className="px">px</span>
+          <span className={toggle ? "rem" : "px"}>{toggle ? "rem" : "px"}</span>
         </div>
-        <div className="rem-box">
+        <div className={toggle ? "px-box" : "rem-box"}>
           <h3>{toggle ? "Pixel" : "Rem"}</h3>
           <input
             type="text"
-            name="rem"
-            value={isNaN(rem) ? "" : rem}
+            value={remInputValue}
             onChange={toggle ? handlePixelChange : handleRemChange}
           />
-          <span className="rem">rem</span>
+          <span className={toggle ? "px" : "rem"}>{toggle ? "px" : "rem"}</span>
         </div>
+      </div>
+      <div className="btn">
         <button className="change" onClick={handleChangeCalc}>
-          바꿔서 계산하기
+          {toggle ? "Pixel to Rem" : "Rem to Pixel"}
         </button>
       </div>
     </div>
